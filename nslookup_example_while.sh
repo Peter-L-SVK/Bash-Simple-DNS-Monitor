fbe3bfbf4af1c03b81b235bc999b3ee20c88477a
Peter-L-SVK <64000980+Peter-L-SVK@users.noreply.github.com>
2020-10-18 14:53:32 +0200

    Add files via upload

---
+++ nslookup_example_while.sh
@@ -0,0 +1,48 @@
+#!/usr/bin/env bash
+
+# Declaration of IPv4 set for control of address validity:
+octet="(25[0-5]|2[0-4][0-9]|[01]?[0-9]?[0-9])"
+ip4="^$octet\\.$octet\\.$octet\\.$octet$"
+
+# Declaration of function we call in body of script:
+function trap_signals
+{
+    trap '{ echo ""; echo "Script interupted by user."; sleep 1.5; 
+    	 exit $true; EXIT_SUCCESS=$?; echo $EXIT_SUCCESS 0; }' INT
+    trap '{ echo "";  echo "Script paused by user."; }' TSTP
+}
+
+function dns_resolve
+{
+    read -p "Enter the name of a DNS server: " dns_address
+    if [ "$dns_address" == "" ]
+    then
+	echo "Error, file must have a name!"
+	exit 1
+    fi
+}
+
+# Here where body starts:
+printf "Exit by Ctrl-C.\n"
+dns_resolve
+
+while $true:
+do
+    trap_signals
+    answer=$(nslookup $dns_address | grep Address | awk 'NR == 2 { print $2; }' )
+    if ([ -z $answer ])
+    then
+	echo "---------------------------------------------------------------"
+	echo "$(date): -> $dns_address is >>DOWN<<. :-("
+    else
+	if [[ $answer =~ $ip4 ]]
+	then
+            echo "---------------------------------------------------------"
+	    echo "$(date): -> $dns_address is UP. :-)"
+	else
+	    echo "---------------------------------------------------------------"
+            echo "$(date): -> $dns_address is >>DOWN<<. :-("
+	fi
+    fi
+    sleep 3
+done
